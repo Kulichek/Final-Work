6c8beb116089ca989f6174549b670fd23013776e
kulichek <kulichek@icloud.com>
2023-06-19 21:43:55 +0300

    написала программу к задаче

--- finalwork2/Program.cs
+++ finalwork2/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Work!");
+﻿//Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
+  //либо задать на старте выполнения алгоритма.
+ //При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+ 
+//Примеры:[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+//[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+//[“Russia”, “Denmark”, “Kazan”] → []
+
+Console.Write("Введите элементы массива через пробел: ");
+string[] array = Console.ReadLine().Split(' ');
+
+string[] shorterArray = new string[array.Length];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+  if (array[i].Length <= 3)
+  {
+    shorterArray[count] = array[i];
+    count++;
+  }
+}
+Array.Resize(ref shorterArray, count);
+
+Console.WriteLine("Новый массив: ");
+foreach (string s in shorterArray)
+{
+  Console.Write(s + " ");
+}
